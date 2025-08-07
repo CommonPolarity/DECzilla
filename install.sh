04fc63d79e2a812b93d6c0ec520dc670c854d3c4
Pascal <jpaccjpoff@gmail.com>
2025-08-06 21:27:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  Installer for
+#  Installation script for
 # __/\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\________/\\\\\\\\\______________________/\\\\\\_____/\\\\\\___________________        
 # _\/\\\////////\\\__\/\\\///////////______/\\\////////______________________\////\\\____\////\\\___________________       
 #  _\/\\\______\//\\\_\/\\\_______________/\\\/__________________________/\\\____\/\\\_______\/\\\___________________      
@@ -69,3 +69,85 @@ sudo cp "$SCRIPT_DIR/requirements.txt" requirements.txt
 
 # Install Python dependencies
 python3 -m pip install -r requirements.txt
+echo "QDEC installation complete!"
+echo "Press any key to continue installation."
+read -s -n 1
+
+# Open port for server use on other devices
+clear
+echo "Would you like to allow setup to open up the port necessary for running QDEC so that you're able to use the hosted site on other devices?"
+echo "This is highly recommended to prevent tedious usage."
+read -p "Open port? [y/n]: " port
+
+# Port logic
+if [[ "$port" = "y" ]]; then
+    sudo ufw allow 8080
+    echo "Port opened successfully!"
+elif [[ "$port" = "n" ]]; then
+    echo "Port not opened. You will need to open it yourself if you want to use QDEC on other devices."
+else
+    echo "Port not opened due to incoherent response. You will need to open it yourself if you want to use QDEC on other devices."
+fi
+echo "Press any key to continue installation."
+read -s -n 1
+
+
+# X Server Configuration Installation
+clear
+echo "We will now begin the installation of the X Server configuration provided."
+echo "QDEC allows for you to send (and if wanted) receive alerts."
+echo "Press any key to continue installation."
+read -s -n 1
+
+# Change directory to `~` and replace script
+cd ~
+sudo cp "$SCRIPT_DIR/.xinitrc" .xinitrc
+echo "X Server configuration installation complete!"
+echo "Press any key to continue installation."
+
+# Line addition to automatically start (Optional, recommended)
+clear
+echo "If you would like to, you are able to automatically start QDEC at login."
+echo "It is highly suggested that you do it, as this makes the process of starting QDEC much simpler."
+read -p "Would you like to enable this? [y/n]: " autostart
+
+# Autostart logic
+if [[ "$autostart" = "y" ]]; then
+    cd ~
+    echo "xstart" >> .profile
+    echo "QDEC autostart added!"
+    echo "Press any key to continue installation."
+    read -s -n 1
+elif [[ "$autostart" = "n" ]]; then
+    echo "QDEC autostart not added."
+    echo "Press any key to continue installation."
+    read -s -n 1
+else
+    cd ~
+    echo "xstart" >> .profile
+    echo "Incoherent answer, QDEC autostart automatically added."
+    echo "If you do not want this, install nano if not installed, cd into ~ and run the following command post-setup:"
+    echo "sudo nano .profile"
+    echo "and remove the line that says 'xstart' before saving."
+    echo "Press any key to continue installation."
+    read -s -n 1
+fi
+
+# Installation finished
+clear
+echo "You have finished the installation to make your own DECzilla."
+read -p "Would you like to restart this computer for safe measures? [y/n]: " restart
+
+# Restart logic
+if [[ "$autostart" = "y" ]]; then
+    echo "Restarting..."
+    sudo shutdown -r now
+elif [[ "$autostart" = "n" ]]; then
+    echo "Computer will not restart."
+else
+    cd ~
+    echo "xstart" >> .profile
+    echo "Incoherent answer, computer will not restart."
+fi
+echo "Exiting installation..."
+exit 1
